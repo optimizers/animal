fd4d9b3e0e19cf282821d64f9b11aec00b5660d9
Alexis Montoison <alexis.montoison@polymtl.ca>
2021-02-25 18:17:29 -0500

    Solutions generated with MINRES-QLP

---
+++ julia/write_mls.jl
@@ -0,0 +1,35 @@
+using BenchmarkTools, HarwellRutherfordBoeing
+using LinearAlgebra, SparseArrays, DelimitedFiles
+using Krylov, Quadmath
+
+function scale_ls!(A)
+  n, m = size(A)
+  s = ones(m)
+  for j = 1 : m
+    i = A.colptr[j]
+    k = A.colptr[j+1] - 1
+    nj = (i ≤ k) ? norm(A.nzval[i:k]) : 0.0
+    if nj > 0.0
+      A.nzval[i:k] ./= nj
+      s[j] = nj
+    end
+  end
+  return s
+end
+
+for pb in ("small", "small2", "medium", "medium2", "large", "large2", "very", "very2")
+  print("$pb: ")
+  A = RutherfordBoeingData("../rb/$(pb).rb").data
+  b = HarwellBoeingMatrix("../hb/$(pb).hb").rhs[:]
+  s = scale_ls!(A)
+  A = Float128.(A)
+  b = Float128.(b)
+  M = A' * A
+  rhs = A' * b
+  x, stats = minres_qlp(M, rhs, rtol=Float128(1e-20), atol=Float128(1e-25))
+  println("✓")
+  x = Float64.(x)
+  open("../mls/krylov/$(pb)_scaled_mls.txt", "w") do io
+    writedlm(io, x)
+  end
+end
